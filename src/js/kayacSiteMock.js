e33ad1d8341b45b8b4bdee55dadf4780213d8445
fujisawa-shin <fujisawa-shin@kayac.com>
2016-03-29 19:16:07 +0900

    ざっくりできた

--- src/js/kayacSiteMock.js
+++ src/js/kayacSiteMock.js
@@ -1,10 +1,30 @@
-import Sample from './lib/Sample';
 import $ from 'jquery';
+import _ from 'lodash';
 
-const sample = new Sample({
-    name: 'world'
+const $root = $('.js-member-root');
+const $more = $('.js-more');
+
+const render = _.template($('#template-member').html());
+
+let currentPage = 0;
+
+function loadMore () {
+    currentPage++;
+    $.ajax(`/api/member.${currentPage}.json`)
+        .done((json) => {
+            _.each(json, (member) => {
+                $root.append(render(member));
+            });
+        })
+        .fail((err) => {
+            $more.hide();
+            alert('こちらが最後のページです。');
+        });
+}
+
+$more.on('click', (e) => {
+    e.preventDefault();
+    loadMore();
 });
 
-$('.wrapper').on('click', () => {
-    console.log(`hello, ${sample.name}.`);
-});
+loadMore();
